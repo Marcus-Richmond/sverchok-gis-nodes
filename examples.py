c300558f4330104acee756fe84198d7619184e8f
Dealga McArdle <tetha.z@gmail.com>
2022-04-15 13:39:13 +0200

    small idiom push

--- examples.py
+++ examples.py
@@ -7,12 +7,10 @@ import sverchok_gis_nodes
 EXAMPLES_PATH = Path(sverchok_gis_nodes.__file__).parent / 'json_examples'
 
 def example_categories_names():
-    names = []
     for category_path in EXAMPLES_PATH.iterdir():
         if category_path.is_dir():
-            names.append((EXAMPLES_PATH, category_path.name))
-    for name in names:
-        yield name
+            yield (EXAMPLES_PATH, category_path.name)
+
 
 def register():
     submenu_classes = (make_submenu_classes(path, category_name) for path, category_name in example_categories_names())
