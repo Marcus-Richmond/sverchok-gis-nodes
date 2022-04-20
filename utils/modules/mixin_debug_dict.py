dc9a0db76b9c53f1eeb522cd6f83bc7a592818e4
Dealga McArdle <tetha.z@gmail.com>
2022-04-20 16:48:32 +0200

    remove some whitespace and tabs

--- utils/modules/mixin_debug_dict.py
+++ utils/modules/mixin_debug_dict.py
@@ -3,14 +3,13 @@ import bpy
 from sverchok.data_structure import node_id
 
 class SvSGNDebugMixinDict():
-	"""
-	the goal of this mixin is to abstract away the node_dict creation.
-	user/node creator should be able to just assume a node dict will be created
-	correctly, and reset if necessary.
-	"""
+    """
+    the goal of this mixin is to abstract away the node_dict creation.
+    user/node creator should be able to just assume a node dict will be created
+    correctly, and reset if necessary.
+    """
     sgn_debug_dict = dict()    # special kind of variable.
     sgn_debug_mode: bpy.types.BoolProperty(default=False, name="debug mode", description="debug switch for node")
-    
 
     def get_current_debug_dict(self):
         self.n_id = node_id(self)
@@ -20,7 +19,7 @@ class SvSGNDebugMixinDict():
             self.sgn_debug_dict[self.n_id] = {}
             found_dict = self.sgn_debug_dict[self.n_id]
         return found_dict
- 
+
     def clear_current_debug_dict(self):
         self.n_id = node_id(self)
         self.sgn_debug_dict[self.n_id] = {}
